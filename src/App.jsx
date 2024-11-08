d24243b208668c6e675c7da57e08dc2361cda517
hunrayy <henryokiyi8@gmail.com>
2024-11-08 13:12:49 +0100

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,6 @@ function App() {
             <Route path="*" element={<PageNotFound />} />
           </Routes>
           <ToastContainer />
-          {/* test code  */}
         </CartProvider>
       </AuthProvider>
     </>
